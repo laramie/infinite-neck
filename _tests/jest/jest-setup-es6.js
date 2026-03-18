aefa694a1e6bf79e82065e255cdf899c4a0f00ef
Laramie Crocker <git@laramiecrocker.com>
2026-03-18 09:53:09 -0700

    removed import, but soon removing this module.

--- _tests/jest/jest-setup-es6.js
+++ _tests/jest/jest-setup-es6.js
@@ -1,6 +1,5 @@
 // Lightweight test helper barrel for Song-focused Jest files.
 // Not currently used in song tests, but perhaps useful when setting up more song tests. to be evaluated again after those tests are implemented.
-export { makeSong } from '../../song.js';
 export {
     setupSongTests,
     getSong,
