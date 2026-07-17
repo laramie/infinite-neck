afcb0930fc4233a214868d67cb96f693bd069e68
Laramie Crocker <git@laramiecrocker.com>
2026-07-17 05:38:13 -0700

    Building tutorials

---
+++ bin/_tutorial-copy-Downloads-to-songs.sh
@@ -0,0 +1,16 @@
+export SRC=~/Downloads
+export DEST=~/infinite-neck/songs
+
+mv $SRC/L001-1.json $DEST/C000-intro/L001-one-string-intro/
+mv $SRC/L002-1.json $DEST/C000-intro/L002-bass-intro/
+mv $SRC/L003-1.json $DEST/C000-intro/L003-guitar-intro/
+mv $SRC/L004-1.json $DEST/C000-intro/L004-piano-intro/
+mv $SRC/L101-1.json $DEST/C100-cowboy-chords/L101-cowboy-intro/
+mv $SRC/L110-1.json $DEST/C100-cowboy-chords/L110-cheatin-heart/
+mv $SRC/L112-1.json $DEST/C100-cowboy-chords/L112-brown-eyed-girl/
+mv $SRC/L201-1.json $DEST/C200-blues/L201-blues-intro/
+mv $SRC/L202-1.json $DEST/C200-blues/L202-mustang-sally/
+mv $SRC/L310-1.json $DEST/C300-rock/L310-smoke-on-the-water/
+mv $SRC/L302-1.json $DEST/C301-jazz-intro/L302-st-louis-woman/
+mv $SRC/L310-1.json $DEST/C301-jazz-intro/L310-autumn-leaves/
+
