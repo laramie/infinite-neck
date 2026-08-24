abe029f42fde909415ed44ac8ac21316d8cc0945
Laramie Crocker <git@laramiecrocker.com>
2026-08-24 05:39:39 -0700

    added key-signatures.json song to show sharps and flats for each key on a 2-fret 4ths calculator; added build"

--- bin/build.sh
+++ bin/build.sh
@@ -20,7 +20,7 @@ npm run package:deploy
 
 ## Your ssh login here:
 export SSH_LAR=    
-export DEPLOY_DATE='20260823-221458'
+export DEPLOY_DATE='20260823-223859'
 
 scp dist/infinite-neck-$DEPLOY_DATE.tar.gz $SSH_LAR@demo.laramiecrocker.com:/home/laramiessh/sites/demo.laramiecrocker.com/
 
@@ -31,7 +31,7 @@ ssh $SSH_LAR@demo.laramiecrocker.com
 ## Adjust the dates, and do something like: 
 
 export DEPLOY_VERSION='v2.1-beta-11'
-export DEPLOY_DATE='20260823-221458'
+export DEPLOY_DATE='20260823-223859'
 
 cd sites/demo.laramiecrocker.com
 mkdir infinite-neck-deploy
